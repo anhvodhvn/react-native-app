54a623ad8a587badea827423a9529d192de341fa
anhvocap <anh.vo@capgemini.com>
2022-01-04 10:37:32 +0700

    - clean up structure folder

--- AppLogistic/screens/index.js
+++
@@ -1,7 +0,0 @@
-import Home from './home';
-import Welcome from './welcome';
-import Delivery from './delivery';
-import Order from './order';
-import Restaurant from './restaurant';
-
-export {Home, Welcome, Delivery, Order, Restaurant};
